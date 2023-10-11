7c2a9afe91ee45d58690f1062b00e2564eacf6d1
mariamelgendy <mariamelgendy30@gmail.com>
2023-10-11 22:20:55 +0300

    DONE

--- root/server/controllers/patientController.js
+++ root/server/controllers/patientController.js
@@ -107,38 +107,97 @@ const createPatient = async (req, res) => {
  };
 
  const filterDoctors = async (req, res) => {
- const { specialty, date, time } = req.body;
-
+ 
   try {
-    const allDoctors = await Doctor.find({ specialty });
-    const availableDoctors = await Promise.all(
-      allDoctors.map(async (doctor) => {
-        if (date && time) {
-          // Check if there is an appointment at the specified date and time
-          const appointment = await Appointment.findOne({
-            doctor: doctor._id,
-            date,
-            isAvailable: true,
-          });
-          if (appointment) {
-            return null; // Doctor is not available
-          }
-        }
-        return doctor; // Doctor is available
-      })
-    );
-    const filteredDoctors = availableDoctors.filter(doctor => doctor !== null);
 
-    if (filteredDoctors.length === 0) {
-      res.status(404).json({ error: 'No available doctors found' });
-    } else {
-      res.status(200).json(filteredDoctors);
-    }
+    const specialty = req.body.specialty
+
+    const date = req.body.date;
+    const appointmentsByDate = await Appointments.find({date : date,  isAvailable : true});
+    const filter = {};
+    const doctorIDS = [];
+
+
+
+
+appointmentsByDate.forEach((appointment) => {
+  if (appointment.isAvailable === true) {
+    doctorIDS.push(appointment.doctor);
+  }
+});
+
+if (specialty) {
+  filter.specialty = specialty;
+}
+
+if (doctorIDS.length > 0) {
+  filter._id = { $in: doctorIDS };
+}
+
+try {
+  const doctors = await Doctor.find(filter).exec();
+  res.json(doctors);
+} catch (err) {
+  console.error(err);
+  res.status(500).json({ error: 'Server error' });
+}
+
+    // const doctorFound =  Doctor.find(filter);
+    // console.log("DOCTORRRRR FOUNDDDDD" + doctorFound);
+
+    
+
+    // // const doctorID = await Appointments.doctor.find({date : date, isAvailable : true});
+    // // console.log(doctorID);
+    // res.json(doctorFound);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // const availableDoctors = await Promise.all(
+    //   allDoctors.map(async (doctor) => {
+    //     if (date && time) {
+    //       // Check if there is an appointment at the specified date and time
+    //       const appointment = await Appointments.findOne({
+    //         doctor: doctor._id,
+    //         date,
+    //         isAvailable: true,
+    //       });
+    //       if (appointment) {
+    //         return null; // Doctor is not available
+    //       }
+    //     }
+    //     return doctor; // Doctor is available
+    //   })
+    // );
+  //   const filteredDoctors = availableDoctors.filter(doctor => doctor !== null);
+
+  //   if (filteredDoctors.length === 0) {
+  //     res.status(404).json({ error: 'No available doctors found' });
+  //   } else {
+  //     res.status(200).json(filteredDoctors);
+  //   }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Server error' });
-  }
-};
+  // }
+};}
 
 const selectDoctor = async (req, res) => {
   // const patiendID = req.params.id;
