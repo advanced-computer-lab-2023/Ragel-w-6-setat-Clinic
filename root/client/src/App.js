4dd9777eb9063760085c35a372fb1000786833b7
LujainTa <lojaintarek222@gmail.com>
2023-10-12 06:35:01 +0200

    1

--- root/client/src/App.js
+++ root/client/src/App.js
@@ -1,1 +1,19 @@
 
+import { Route, Routes } from "react-router-dom";
+//import Login from "./components/Login";
+
+function App() {
+    // return (
+    // //   <Routes>
+    // //     <Route path="/" exact element={<Login />} />
+    // //     <Route path="/registration" element={<RegistrationOptions />} />
+    // //     <Route path="/registration/patient" element={<PatientRegistration />} />
+    // //     <Route path="/registration/doctor" element={<DoctorRegistration />} />
+    // //     <Route path="/admin/landingpage" element={<AdminLanding />} />
+    // //     <Route path="/admin/addadmin" element={<AddAdmin />} />
+    // //     <Route path="/admin/removeuser" element={<RemoveUser />} />
+    // //   </Routes>
+    // );
+  }
+  
+  export default App;
